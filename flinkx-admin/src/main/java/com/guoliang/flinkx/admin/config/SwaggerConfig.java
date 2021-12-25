e9e424aa2d5347442da7756e384e50018262ff49
jianchun <jc33618@163.com>
2021-12-25 12:54:04 +0800

    swagger优化

--- flinkx-admin/src/main/java/com/guoliang/flinkx/admin/config/SwaggerConfig.java
+++ flinkx-admin/src/main/java/com/guoliang/flinkx/admin/config/SwaggerConfig.java
@@ -6,12 +6,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -27,7 +33,21 @@ public class SwaggerConfig implements WebMvcConfigurer {
 
     @Bean
     public Docket createRestApi() {
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+        ParameterBuilder ticketParam = new ParameterBuilder();
+        List<Parameter> params = new ArrayList<>();
+        ticketParam.name("Authorization")
+                .description("Authorization")
+                .defaultValue("test Authorization info")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(true)
+                .build();
+        params.add(ticketParam.build());
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .globalOperationParameters(params)
+                .select()
                 .apis(RequestHandlerSelectors.basePackage("com.guoliang.flinkx.admin.controller")).paths(PathSelectors.any())
                 .build();
     }
